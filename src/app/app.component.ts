69510b4f067bed628c3c9ed18be2d89a73049e30
hawrot <mhawro200@caledonian.ac.uk>
2020-05-28 22:53:36 +0100

    Fetch accounts from the service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AccountsService} from "./accounts.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AccountsService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  accounts : {name: string, status: string}[] = [];
 
-
-  onAccountAdded(newAccount: {name: string, status: string}) {
+  constructor(private accountsService: AccountsService) {
 
   }
 
-  onStatusChanged(updateInfo: {id: number, newStatus: string}) {
-    this.accounts[updateInfo.id].status = updateInfo.newStatus;
+  ngOnInit(): void {
+    this.accounts = this.accountsService.accounts;
   }
 }
