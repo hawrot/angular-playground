4ec9acad870338bc1e9dba9dccc851b10a5c7d5f
hawrot <mhawro200@caledonian.ac.uk>
2020-05-28 22:58:46 +0100

    Inject Account service and its method into components

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,26 +1,26 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {Component} from '@angular/core';
 import {LoggingService} from "../logging.service";
+import {AccountsService} from "../accounts.service";
 
 
 @Component({
-  selector: 'app-new-account',
-  templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+    selector: 'app-new-account',
+    templateUrl: './new-account.component.html',
+    styleUrls: ['./new-account.component.css'],
+    providers: [LoggingService, AccountsService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+    constructor(
+        private loggingService: LoggingService,
+        private accountsService: AccountsService
+    ) {
+
+    }
 
 
-  constructor(private loggingService: LoggingService) {
-
-  }
-
-  onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    this.loggingService.logStatusChange(accountStatus);
-  }
+    onCreateAccount(accountName: string, accountStatus: string) {
+        this.accountsService.addAccount(accountName, accountStatus);
+        this.loggingService.logStatusChange(accountStatus);
+    }
 }
