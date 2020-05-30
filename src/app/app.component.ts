ca9eda4f23c54a7694132f2dd32bf3a8388c23ad
hawrot <mhawro200@caledonian.ac.uk>
2020-05-30 22:04:52 +0100

    Add observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,27 @@
-import {Component, OnInit} from '@angular/core';
-import {AccountsService} from "./accounts.service";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-
 })
-export class AppComponent implements OnInit{
-  accounts : {name: string, status: string}[] = [];
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated = false;
+  private activatedSub: Subscription;
 
-  constructor(private accountsService: AccountsService) {
-
+  constructor(private userService: UserService) {
   }
 
-  ngOnInit(): void {
-    this.accounts = this.accountsService.accounts;
+  ngOnInit() {
+    this.activatedSub = this.userService.activatedEmitter.subscribe(didActivate => {
+      this.userActivated = didActivate;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.activatedSub.unsubscribe();
   }
 }
